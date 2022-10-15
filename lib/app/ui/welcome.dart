18f10e1f18d74ce7bca3def9fbe423f8e6ee6831
kevin2-cyber <keduful@st.ug.edu.gh>
2022-10-15 10:00:10 +0000

    added background image

--- lib/app/ui/welcome.dart
+++ lib/app/ui/welcome.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:live_me/utils/app_assets.dart';
 
 class Welcome extends StatelessWidget {
   const Welcome({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(AppAssets.kWelcomeBg),
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,4 @@
+class AppAssets{
+
+  static String kWelcomeBg = 'assets/images/welcome_bg.jpeg';
+}
