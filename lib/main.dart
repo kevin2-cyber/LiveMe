0588ac687656cb09eaf983592000defb2f6b1fca
kevin2-cyber <keduful@st.ug.edu.gh>
2022-11-04 08:11:39 +0000

    added device_preview package

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,28 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:live_me/app/app.dart';
 
-void main(){
-
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   ErrorWidget.builder = (FlutterErrorDetails details) => Material(
-    color: Colors.white70,
-    child: Center(
-      child: Text(
-        details.exception.toString(),
-        style: const TextStyle(
-          color: Colors.black38,
-          fontWeight: FontWeight.w300,
-          fontSize: 18,
+        color: Colors.white70,
+        child: Center(
+          child: Text(
+            details.exception.toString(),
+            style: const TextStyle(
+              color: Colors.black38,
+              fontWeight: FontWeight.w300,
+              fontSize: 18,
+            ),
+          ),
         ),
-      ),
-    ),
-  );
+      );
 
-  runApp(const LiveMeApp());
+  runApp(
+      DevicePreview(
+        enabled: !kReleaseMode,
+          builder: (context) => const LiveMeApp())
+  );
 }
