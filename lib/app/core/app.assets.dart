e3aa98995121416d86188914b66e6d6280da092d
kevin2-cyber <keduful@st.ug.edu.gh>
2022-11-08 09:36:30 +0000

    new

--- lib/app/core/app.assets.dart
+++ lib/app/core/app.assets.dart
@@ -1,5 +1,4 @@
 class AppAssets{
-
   static String kWelcomeBg = 'assets/images/welcome_bg.jpeg';
   static String kCommentIcon = 'assets/icons/comment.png';
   static String kMedalIcon = 'assets/icons/medal.png';
