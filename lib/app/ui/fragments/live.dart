cb7959977f37983c2e7f1c811b3a2a1aa5601476
kevin2-cyber <keduful@st.ug.edu.gh>
2022-11-08 10:19:38 +0000

    new

--- lib/app/ui/fragments/live.dart
+++ lib/app/ui/fragments/live.dart
@@ -8,8 +8,33 @@ class LiveFragment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: const [
-        LiveMeAppBar(appBarColor: Colors.transparent,),
+      children: [
+        const LiveMeAppBar(appBarColor: Colors.transparent),
+        // ListView(
+        //   scrollDirection: Axis.horizontal,
+        //   children: [
+        //     Container(
+        //       height: 200,
+        //       width: 200,
+        //       color: Colors.amber,
+        //     ),
+        //     Container(
+        //       height: 200,
+        //       width: 200,
+        //       color: Colors.amberAccent,
+        //     ),
+        //     Container(
+        //       height: 200,
+        //       width: 200,
+        //       color: Colors.black12,
+        //     ),
+        //     Container(
+        //       height: 200,
+        //       width: 200,
+        //       color: Colors.black54,
+        //     ),
+        //   ],
+        // ),
       ],
     );
   }
