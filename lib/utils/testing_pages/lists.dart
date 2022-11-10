108e6fea96cf132f14bae5fbff20891c1cbde444
kevin2-cyber <keduful@st.ug.edu.gh>
2022-11-10 07:34:13 +0000

    working on lists

---
+++ lib/utils/testing_pages/lists.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ListViewSample extends StatelessWidget {
+  const ListViewSample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
