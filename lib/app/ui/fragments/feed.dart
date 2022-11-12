3932f5e0b35fa37f24830908a839ef9b5f9cb20e
kevin2-cyber <keduful@st.ug.edu.gh>
2022-11-12 15:18:30 +0000

    almost done implementing for feed fragment

--- lib/app/ui/fragments/feed.dart
+++ lib/app/ui/fragments/feed.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:live_me/app/widgets/app.bar.dart';
 
+import '../../widgets/scroll.card.dart';
+
 class FeedFragment extends StatelessWidget {
   const FeedFragment({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: const [
+      children:[
         LiveMeAppBar(
           appBarColor: Colors.black,
           iconColor: Colors.white,
         ),
+        ListView(
+          children: [
+          ScrollCard()
+        ]),
       ],
     );
   }
 }
+
+
