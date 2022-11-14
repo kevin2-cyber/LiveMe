cfa7581c44e702ef5de8f2a1ebaa90337bbe6a94
kevin2-cyber <keduful@st.ug.edu.gh>
2022-11-14 09:16:13 +0000

    having problems with simulator

--- lib/app/ui/fragments/profile.dart
+++ lib/app/ui/fragments/profile.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:live_me/app/core/core.dart';
 
 class ProfileFragment extends StatelessWidget {
   const ProfileFragment({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          CircleAvatar(
+            backgroundImage: AssetImage(AppAssets.kProfileAvatar),
+          ),
+        ],
+      ),
+    );
   }
 }
