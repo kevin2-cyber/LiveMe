92a82eed399f777909cda4d1ded804452edc01f1
kevin2-cyber <keduful@st.ug.edu.gh>
2022-11-21 13:00:52 +0000

    created a custom switch for the settings screen

--- lib/app/ui/settings.dart
+++ lib/app/ui/settings.dart
@@ -1,10 +1,142 @@
 import 'package:flutter/material.dart';
+import 'package:live_me/app/core/core.dart';
+import 'package:live_me/app/widgets/custom.switch.dart';
 
-class Settings extends StatelessWidget {
+class Settings extends StatefulWidget {
   const Settings({Key? key}) : super(key: key);
 
   @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  bool isSwitched = false;
+  bool isOn = false;
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leadingWidth: 100,
+        elevation: 1,
+        backgroundColor: Colors.white,
+        title: const Text(
+            'Settings',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Row(
+            children: const [
+              Icon(
+                Icons.arrow_back_ios_new,
+                color: Colors.blue,
+              ),
+              Text(
+                'Back',
+                style: TextStyle(
+                  color: Colors.blue,
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: Column(
+                children: const [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    'Cancel',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const [
+                Text(
+                    'Privacy',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black54,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: AppConstants.sizeConfig(context, 0.01).height,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children:  [
+                const Text('Private Account'),
+                CustomSwitch(
+                    value: isSwitched,
+                    onChanged: (value) {
+                      setState(() {
+                        isSwitched = value;
+                      });
+                    },
+                ),
+              ],
+            ),
+            SizedBox(
+              height: AppConstants.sizeConfig(context, 0.01).height,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children:  [
+                const Text('Private Follow List'),
+                CustomSwitch(
+                    value: isOn,
+                    onChanged: (value) {
+                      setState(() {
+                        isOn = value;
+                      });
+                    },
+                ),
+              ],
+            ),
+            SizedBox(
+              height: AppConstants.sizeConfig(context, 0.01).height,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const [
+                Text(
+                    'User Settings',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black54,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
