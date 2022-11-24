4cdfcbf4814c1069a5ba36f41342f57b502dd899
kevin2-cyber <keduful@st.ug.edu.gh>
2022-11-24 08:47:33 +0000

    done with followers ui

--- lib/app/ui/followers.dart
+++ lib/app/ui/followers.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:live_me/app/core/core.dart';
+
+import '../widgets/custom.list.tile.dart';
 
 class Followers extends StatelessWidget {
   const Followers({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var kBorderRadius = BorderRadius.circular(10);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -35,6 +39,165 @@ class Followers extends StatelessWidget {
           ),
         ),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(14.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: AppConstants.sizeConfig(context, 0.02).height,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: kBorderRadius,
+                color: AppConstants.kEditTextNormal,
+              ),
+              child: TextFormField(
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(
+                      Icons.search,
+                    color: Colors.grey,
+                  ),
+                  label: const Text(
+                      'Search',
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: kBorderRadius,
+                    borderSide: BorderSide.none,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: kBorderRadius,
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+            ),
+            const Divider(),
+            Container(
+              height: AppConstants.sizeConfig(context, 0.765).height,
+              child: ListView(
+                scrollDirection: Axis.vertical,
+                children:  [
+                    CustomListTile(
+                      image: AppAssets.kListAvatar,
+                    ),
+                    SizedBox(
+                      height: AppConstants.sizeConfig(context, 0.01).height,
+                    ),
+                    CustomListTile(
+                      image: AppAssets.kListAvatar1,
+                    ),
+                    SizedBox(
+                      height: AppConstants.sizeConfig(context, 0.01).height,
+                    ),
+                    CustomListTile(
+                      image: AppAssets.kListAvatar1,
+                    ),
+                    SizedBox(
+                      height: AppConstants.sizeConfig(context, 0.01).height,
+                    ),
+                    CustomListTile(
+                      image: AppAssets.kListAvatar1,
+                    ),
+                    SizedBox(
+                      height: AppConstants.sizeConfig(context, 0.01).height,
+                    ),
+                    CustomListTile(
+                      image: AppAssets.kListAvatar1,
+                    ),
+                    SizedBox(
+                      height: AppConstants.sizeConfig(context, 0.01).height,
+                    ),
+                    CustomListTile(
+                      image: AppAssets.kListAvatar1,
+                    ),
+                    SizedBox(
+                      height: AppConstants.sizeConfig(context, 0.01).height,
+                    ),
+                    CustomListTile(
+                      image: AppAssets.kListAvatar1,
+                    ),
+                    SizedBox(
+                      height: AppConstants.sizeConfig(context, 0.01).height,
+                    ),
+                    CustomListTile(
+                      image: AppAssets.kListAvatar1,
+                    ),
+                    SizedBox(
+                      height: AppConstants.sizeConfig(context, 0.01).height,
+                    ),
+                    CustomListTile(
+                      image: AppAssets.kListAvatar1,
+                    ),
+                    SizedBox(
+                      height: AppConstants.sizeConfig(context, 0.01).height,
+                    ),
+                    CustomListTile(
+                      image: AppAssets.kListAvatar1,
+                    ),
+                    SizedBox(
+                      height: AppConstants.sizeConfig(context, 0.01).height,
+                    ),
+                    CustomListTile(
+                      image: AppAssets.kListAvatar1,
+                    ),
+                    SizedBox(
+                      height: AppConstants.sizeConfig(context, 0.01).height,
+                    ),
+                    CustomListTile(
+                      image: AppAssets.kListAvatar1,
+                    ),
+                    SizedBox(
+                      height: AppConstants.sizeConfig(context, 0.01).height,
+                    ),
+                    CustomListTile(
+                      image: AppAssets.kListAvatar1,
+                    ),
+                    SizedBox(
+                      height: AppConstants.sizeConfig(context, 0.01).height,
+                    ),
+                    CustomListTile(
+                      image: AppAssets.kListAvatar1,
+                    ),
+                    SizedBox(
+                      height: AppConstants.sizeConfig(context, 0.01).height,
+                    ),
+                    CustomListTile(
+                      image: AppAssets.kListAvatar1,
+                    ),
+                    SizedBox(
+                      height: AppConstants.sizeConfig(context, 0.01).height,
+                    ),
+                    CustomListTile(
+                      image: AppAssets.kListAvatar1,
+                    ),
+                    SizedBox(
+                      height: AppConstants.sizeConfig(context, 0.01).height,
+                    ),
+                    CustomListTile(
+                      image: AppAssets.kListAvatar1,
+                    ),
+                    SizedBox(
+                      height: AppConstants.sizeConfig(context, 0.01).height,
+                    ),
+                    CustomListTile(
+                      image: AppAssets.kListAvatar1,
+                    ),
+                    SizedBox(
+                      height: AppConstants.sizeConfig(context, 0.01).height,
+                    ),
+                  ],
+                ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+
