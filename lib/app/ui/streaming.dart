a06db312af77e1aa04a694fa9ecda36eb9e18b58
kevin2-cyber <keduful@st.ug.edu.gh>
2022-12-13 22:14:37 +0000

    done with extraction

--- lib/app/ui/streaming.dart
+++ lib/app/ui/streaming.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:live_me/app/core/core.dart';
 
+import '../widgets/side.stream.dart';
+
 class Streaming extends StatelessWidget {
   const Streaming({Key? key}) : super(key: key);
 
@@ -149,36 +151,4 @@ class Streaming extends StatelessWidget {
   }
 }
 
-class SideStream extends StatelessWidget {
-  const SideStream({
-    super.key,
-  });
 
-  @override
-  Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: const BorderRadius.only(
-        topLeft: Radius.circular(20),
-        bottomLeft: Radius.circular(20)
-      ),
-        child: Container(
-          height: AppConstants.sizeConfig(context, 0.18).height,
-          width: AppConstants.sizeConfig(context, 0.12).width,
-          decoration: BoxDecoration(
-            color: const Color(0xFF151515).withAlpha(55),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Image.asset(AppAssets.kSharingT),
-              Image.asset(AppAssets.kDollarT),
-              CircleAvatar(
-                backgroundColor: const Color(0xFF7C7C7C).withAlpha(100),
-                child: Image.asset(AppAssets.kChevronUp),
-              ),
-            ],
-          ),
-        ),
-    );
-  }
-}
