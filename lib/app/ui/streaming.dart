a55416344beed938ebe7359788456d3d76783352
kevin2-cyber <keduful@st.ug.edu.gh>
2022-12-16 05:06:50 +0000

    extracted the text suggestions

--- lib/app/ui/streaming.dart
+++ lib/app/ui/streaming.dart
@@ -159,85 +159,7 @@ class Streaming extends StatelessWidget {
             Positioned(
               top: 800,
               left: 20,
-              child: Row(
-                children: [
-                  Container(
-                    width: 70,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      color: withAlpha,
-                      borderRadius: const BorderRadius.all(Radius.circular(20)),
-                    ),
-                    child:  const Center(
-                      child: Text(
-                          'Hello!!',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  Container(
-                    width: 110,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      color: withAlpha,
-                      borderRadius: const BorderRadius.all(Radius.circular(20)),
-                    ),
-                    child:  const Center(
-                      child: Text(
-                        'How\'s you',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 05,
-                  ),
-                  CircleAvatar(
-                    backgroundColor: withAlpha,
-                    child: Image.asset(AppAssets.kEmojiUn),
-                  ),
-                  const SizedBox(
-                    width: 05,
-                  ),
-                  CircleAvatar(
-                    backgroundColor: withAlpha,
-                    child: Image.asset(AppAssets.kEmojiD),
-                  ),
-                  const SizedBox(
-                    width: 05,
-                  ),
-                  CircleAvatar(
-                    backgroundColor: withAlpha,
-                    child: Image.asset(AppAssets.kEmojiT),
-                  ),
-                  const SizedBox(
-                    width: 05,
-                  ),
-                  Container(
-                    width: 70,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      color: withAlpha,
-                      borderRadius: const BorderRadius.all(Radius.circular(20)),
-                    ),
-                    child:  const Center(
-                      child: Text(
-                        'Great',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+              child: TextSuggestions(withAlpha: withAlpha),
             ),
             Positioned(
               top: 850,
@@ -251,6 +173,95 @@ class Streaming extends StatelessWidget {
   }
 }
 
+class TextSuggestions extends StatelessWidget {
+  const TextSuggestions({
+    super.key,
+    required this.withAlpha,
+  });
 
+  final Color withAlpha;
 
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 70,
+          height: 40,
+          decoration: BoxDecoration(
+            color: withAlpha,
+            borderRadius: const BorderRadius.all(Radius.circular(20)),
+          ),
+          child:  const Center(
+            child: Text(
+                'Hello!!',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(
+          width: 10,
+        ),
+        Container(
+          width: 110,
+          height: 40,
+          decoration: BoxDecoration(
+            color: withAlpha,
+            borderRadius: const BorderRadius.all(Radius.circular(20)),
+          ),
+          child:  const Center(
+            child: Text(
+              'How\'s you',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(
+          width: 05,
+        ),
+        CircleAvatar(
+          backgroundColor: withAlpha,
+          child: Image.asset(AppAssets.kEmojiUn),
+        ),
+        const SizedBox(
+          width: 05,
+        ),
+        CircleAvatar(
+          backgroundColor: withAlpha,
+          child: Image.asset(AppAssets.kEmojiD),
+        ),
+        const SizedBox(
+          width: 05,
+        ),
+        CircleAvatar(
+          backgroundColor: withAlpha,
+          child: Image.asset(AppAssets.kEmojiT),
+        ),
+        const SizedBox(
+          width: 05,
+        ),
+        Container(
+          width: 70,
+          height: 40,
+          decoration: BoxDecoration(
+            color: withAlpha,
+            borderRadius: const BorderRadius.all(Radius.circular(20)),
+          ),
+          child:  const Center(
+            child: Text(
+              'Great',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
 
