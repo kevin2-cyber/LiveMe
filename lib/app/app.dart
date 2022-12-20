f7833a9f67554cb941693817de57bbd4e5337f7e
kevin2-cyber <keduful@st.ug.edu.gh>
2022-12-20 17:03:47 +0000

    created the app section

---
+++ lib/app/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class LiveMeApp extends StatelessWidget {
+  const LiveMeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:live_me/app/app.dart';
+
+import 'app/app.dart';
 
 void main() {
 
   ErrorWidget.builder = (FlutterErrorDetails details) => Material(
-        color: Colors.white70,
+        color: CupertinoColors.white,
         child: Center(
           child: Text(
             details.exception.toString(),
             style: GoogleFonts.lexendDeca(
-              color: Colors.black38,
+              color: CupertinoColors.black,
               fontWeight: FontWeight.w300,
               fontSize: 18,
             ),
