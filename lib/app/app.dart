da899d1400dbf522959e1c8e9c42cdc022aac19c
kevin2-cyber <keduful@st.ug.edu.gh>
2022-12-20 17:11:09 +0000

    new

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/cupertino.dart';
+import 'package:live_me/app/ui/welcome.dart';
 
 class LiveMeApp extends StatelessWidget {
   const LiveMeApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp();
+    return const CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      title: 'LiveMe',
+      theme: CupertinoThemeData(
+        brightness: Brightness.light,
+      ),
+      home: Welcome(),
+    );
   }
 }
---
+++ lib/app/ui/welcome.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class Welcome extends StatelessWidget {
+  const Welcome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: Container(),
+    );
+  }
+}
