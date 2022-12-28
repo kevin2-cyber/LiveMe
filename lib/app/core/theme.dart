34b534ed734eb04594b7970bfa948c69d403e6ce
kevin2-cyber <keduful@st.ug.edu.gh>
2022-12-28 10:16:16 +0000

    done with themes

---
+++ lib/app/core/theme.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/cupertino.dart';
+
+  // dark theme
+  CupertinoThemeData kDarkTheme = const CupertinoThemeData(
+    brightness: Brightness.dark,
+  );
+
+  // light theme
+  CupertinoThemeData kLightTheme = const CupertinoThemeData(
+    brightness: Brightness.light,
+  );
