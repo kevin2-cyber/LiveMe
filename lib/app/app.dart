c2ca7073e21fb314eed58ca7919fe693bb1f6ac9
kevin2-cyber <keduful@st.ug.edu.gh>
2022-12-28 10:41:37 +0000

    testing the use of ternary operators

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -5,7 +5,7 @@ class LiveMeApp extends StatelessWidget {
   const LiveMeApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, {bool isDark = false}) {
     return const CupertinoApp(
       debugShowCheckedModeBanner: false,
       title: 'LiveMe',
